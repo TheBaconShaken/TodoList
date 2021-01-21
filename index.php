3ffdb2dc4f400005e27ea6de56e6d894b340e08f
camthulse <hulse.hulse11@gmail.com>
2021-01-21 18:12:59 -0500

    Simple changes.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List</title>
+    <link rel="stylesheet" href="css/style.css" >
 </head>
 <body>
     <!-- Todo List -->
@@ -15,12 +16,20 @@
         </div>
         <ul id="todolist-list">
             <?php
-                $query = "SELECT * FROM todolist;";
+                $query = "SELECT * FROM todos;";
                 $result = mysqli_query($conn, $query);
-                if(!mysqli_num_rows($result)){
-                    echo "No results.";
-                }else{
-                    
+                $resultcheck = mysqli_num_rows($result);
+                
+                if($resultcheck > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+                        ?>
+                            <li class="list-item">
+                                <h1 class="item-title"><?php echo $row['title'];?></h1>
+                                <p class="item-description"><?php echo $row['description'];?></p>
+                                <h2><?php echo $row['status']; ?></h2>
+                            </li>
+                        <?php
+                    }
                 }
             ?>
         </ul>
